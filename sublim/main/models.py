4854c666825044a6f516f9996926fe9bbd17230e
Sapronov-Nikolay <avindialit@list.ru>
2023-12-17 00:49:39 +0300

    начали процесс добавления фото

--- sublim/main/models.py
+++ sublim/main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 
 class Good(models.Model):
+    picture = models.ImageField()
     namegood = models.CharField(max_length=30)
     specification = models.CharField(max_length=300)
     price = models.FloatField()
