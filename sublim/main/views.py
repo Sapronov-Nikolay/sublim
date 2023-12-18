0ad3bddd6bf87b9bdbea99ad428fe77d9dcafe39
Sapronov-Nikolay <avindialit@list.ru>
2023-12-18 22:29:37 +0300

    Синхронизируемся

--- sublim/main/views.py
+++ sublim/main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import *
 from .models import Good
 # Create your views here.
 def index(request):
