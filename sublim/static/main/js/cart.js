65691bdb9c930fde842c2de6b04b8938a0f0fec8
EkaterinaKarelina <katekrln0309@gmail.com>
2024-02-05 21:59:59 +0300

    Создали фун-ю js для корзины с cookie-сами

--- sublim/static/main/js/cart.js
+++ sublim/static/main/js/cart.js
@@ -1,19 +1,19 @@
 function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
-
-function cry(){
-    console.log('Привет, Вера!');
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
-cry();
+
+function add_to_cart(elem) {
+  document.cookie = elem.id + "=" + elem.value;
+}
 
 
-document.cookie='cart={"potato":1,"beetroot",2}'
+
+/* document.cookie='cart={"potato":1,"beetroot",2}'
 var cart = JSON.parse(getCookie("cart"));
 cart['onion']=20;
 20
 JSON.stringify(cart);
-'{"potato":1,"beetroot":2,"onion":20}'
+'{"potato":1,"beetroot":2,"onion":20}' */
